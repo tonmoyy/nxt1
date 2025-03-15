4e6316406931dabcac71167b2c224a322ae64e0e
tonmoyy <hasantonmoy@gmail.com>
2025-03-15 19:51:59 +0600

    canceled.js  is incorporated.

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 export const client = sanityClient({
-        projectId: 'gi5dwggu',
+        projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
         dataset: 'production',
         apiVersion: "2022-11-27",
         useCdn: true,
